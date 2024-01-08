62c9b9b7bd469b8df02046d70dfbfe0f0ae4c100
Surya1810 <suryadinarta8@gmail.com>
2024-01-08 12:27:22 +0700

    update using cdn, delete vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- Our style -->
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
@@ -110,8 +112,9 @@
         </button>
     </main>
 
-    <!-- Select2 -->
-    <script src="{{ asset('assets/select2/js/select2.full.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 
     <script>
         //Back to Top Button
